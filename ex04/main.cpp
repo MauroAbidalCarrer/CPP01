ebaeaf767d938dff85c33fb457f82dac8f2b05cb
Mauro ABIDAL CARRER <mauroabidal@yahoo.fr>
2023-01-30 21:42:58 +0100

    ex04 finished?

---
+++ ex04/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+
+bool setup(int ac, char **av, std::ifstream& srcFile, std::ofstream& dstFile)
+{
+    if (ac != 4)
+    {
+        std::cerr << "ERROR:user should input 3 arguments!" << std::endl;
+        return 1;
+    }
+    std::string fileName = av[1];
+    srcFile.open(fileName.c_str());
+    if (srcFile.fail())
+    {
+        std::cerr << "ERROR:could not open " << fileName << " file!" << std::endl;
+        return true;
+    }
+    fileName += + ".replace";
+    dstFile.open(fileName.c_str(), std::ios::trunc);
+    if (dstFile.fail())
+    {
+        std::cerr << "ERROR:could not open " << fileName << " file!" << std::endl;
+        return true;
+    }
+    return false;
+}
+
+std::string getFileContent(std::ifstream& srcFile)
+{
+	std::stringstream sStream;
+    sStream << srcFile.rdbuf();
+    std::string content = sStream.str();
+    sStream.clear();
+    return content;
+}
+
+void replaceOccurences(std::string& srcFileContent, std::string occurence, std::string newString)
+{
+    size_t pos;
+    while (1)
+    {
+        pos = srcFileContent.find(occurence);
+        if (pos == std::string::npos)
+            break ;
+        srcFileContent.erase(pos, occurence.length());
+        srcFileContent.insert(pos, newString);
+    }
+}
+
+int  main(int ac, char **av)
+{
+    std::ifstream srcFile;
+    std::ofstream dstFile;
+    if (setup(ac, av, srcFile, dstFile))
+        return 1;
+    std::string content = getFileContent(srcFile);
+    replaceOccurences(content, av[2], av[3]);
+    dstFile << content;
+}
